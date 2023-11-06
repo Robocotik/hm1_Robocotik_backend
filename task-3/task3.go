5625878d78c9f081f2e0101252a0a35cf06f4a54
Alexander Starkin <33557475+Robocotik@users.noreply.github.com>
2023-11-06 15:09:05 +0300

    Update task-3/task3.go
    [TASK3] changed by tutor
    
    Co-authored-by: Alexander Timofeev <68970161+bloodNtears@users.noreply.github.com>

--- task-3/task3.go
+++ task-3/task3.go
@@ -11,11 +11,11 @@ func main(){
 	for i:=0; i<n; i++ {
 		fmt.Scanf("%d",&nums[i])
 	}
-	end_el:=nums[n-1]
-	for i:=n - 1; i>0;i--{
+	lastElem := nums[n-1]
+	for i := n - 1; i > 0; i-- {
 		nums[i] = nums[i-1]
 	} 
-	nums[0] = end_el
+	nums[0] = lastElem
 
 	for i:=0; i<n; i++{
 		fmt.Printf("%d ",nums[i])
