1b73cbaaeb267a4858a47a018ea2a8c5beba06d8
Alexander Starkin <33557475+Robocotik@users.noreply.github.com>
2023-11-06 15:15:15 +0300

    [VIEW] space problem done for task4.go

--- task-4/task4.go
+++ task-4/task4.go
@@ -6,12 +6,12 @@ func main(){
 	var n int
 	fmt.Scanf("%d\n", &n)
 	nums := make([][]int, n)
-	for i :=0; i<n; i++ {
+	for i := 0; i < n; i++ {
 		nums[i] = make([]int, n)
 	}
 
-	for i:=0; i<n;i++{
-		for j:=0; j<n;j++{
+	for i := 0; i < n; i++{
+		for j := 0; j < n; j++{
 			fmt.Scanf("%d",&nums[i][j])
 			
 		}
@@ -19,8 +19,8 @@ func main(){
 	}
 
 	flag:= true
-	for i:=0; i<n;i++{
-		for j:=0; j <n; j++{
+	for i := 0; i < n; i++{
+		for j := 0; j < n; j++{
 			if(nums[i][j] != nums[n-1-i][n-1-j]){
 				flag = false
 				break
