4f8269ecfdf9a19ad1a5249adffd19636e9b9f2b
Alexander Starkin <33557475+Robocotik@users.noreply.github.com>
2023-11-06 15:12:13 +0300

    [VIEW] space problem done

--- task-3/task3.go
+++ task-3/task3.go
@@ -6,10 +6,10 @@ func main(){
 	var n int
 
 	fmt.Scanf("%d", &n)
-	nums:=make([]int, n)
+	nums := make([]int, n)
 
-	for i:=0; i<n; i++ {
-		fmt.Scanf("%d",&nums[i])
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &nums[i])
 	}
 	lastElem := nums[n-1]
 	for i := n - 1; i > 0; i-- {
@@ -17,7 +17,7 @@ func main(){
 	} 
 	nums[0] = lastElem
 
-	for i:=0; i<n; i++{
-		fmt.Printf("%d ",nums[i])
+	for i := 0; i < n; i++{
+		fmt.Printf("%d ", nums[i])
 	}
 }
